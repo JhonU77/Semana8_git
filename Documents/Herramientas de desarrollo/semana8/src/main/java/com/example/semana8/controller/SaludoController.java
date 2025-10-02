96fd6f0315f6444bbccae3e9181f4cb8a9a54be9
Dev A <devA@gmail.com>
2025-10-02 12:30:16 -0500

    Agrego controlador de saludo

---
+++ Documents/Herramientas de desarrollo/semana8/src/main/java/com/example/semana8/controller/SaludoController.java
@@ -0,0 +1,13 @@
+package com.example.semana8.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+    @GetMapping("Saludo")
+    public String Saludo(){
+        return "Hola desde Pull Request";
+    }
+    
+}
